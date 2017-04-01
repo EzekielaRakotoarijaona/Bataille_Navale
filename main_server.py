afe1bd9175f32589aebc52ebd4077293b84ae094
camillerakotoarijaona <camille.rakotoarijaona@u-bordeaux.fr>
2017-04-01 19:30:16 +0200

    premier commit

---
+++ main_server.py
@@ -0,0 +1,122 @@
+#!/usr/bin/python3
+
+from game import *
+import  random
+import time
+import socket
+import select
+
+
+
+
+
+
+""" generate a random valid configuration """
+def randomConfiguration():
+    boats = [];
+    while not isValidConfiguration(boats):
+        boats=[]
+        for i in range(5):
+            x = random.randint(1,10)
+            y = random.randint(1,10)
+            isHorizontal = random.randint(0,1) == 0
+            boats = boats + [Boat(x,y,LENGTHS_REQUIRED[i],isHorizontal)]
+    return boats
+
+    
+
+def displayConfiguration(boats, shots=[], showBoats=True):
+    Matrix = [[" " for x in range(WIDTH+1)] for y in range(WIDTH+1)]
+    for i  in range(1,WIDTH+1):
+        Matrix[i][0] = chr(ord("A")+i-1)
+        Matrix[0][i] = i
+
+    if showBoats:
+        for i in range(NB_BOATS):
+            b = boats[i]
+            (w,h) = boat2rec(b)
+            for dx in range(w):
+                for dy in range(h):
+                    Matrix[b.x+dx][b.y+dy] = str(i)
+
+    for (x,y,stike) in shots:
+        if stike:
+            Matrix[x][y] = "X"
+        else:
+            Matrix[x][y] = "O"
+
+
+    for y in range(0, WIDTH+1):
+        if y == 0:
+            l = "  "
+        else:
+            l = str(y)
+            if y < 10:
+                l = l + " "
+        for x in range(1,WIDTH+1):
+            l = l + str(Matrix[x][y]) + " "
+        print(l)
+
+""" display the game viewer by the player"""
+def displayGame(game, player):
+    otherPlayer = (player+1)%2
+    displayConfiguration(game.boats[player], game.shots[otherPlayer], showBoats=True)
+    displayConfiguration([], game.shots[player], showBoats=False)
+
+
+
+""" Play a new random shot """
+def randomNewShot(shots):
+    (x,y) = (random.randint(1,10), random.randint(1,10))
+    while not isANewShot(x,y,shots):
+        (x,y) = (random.randint(1,10), random.randint(1,10))
+    return (x,y)
+
+def main():
+    
+    boats1 = randomConfiguration()
+    boats2 = randomConfiguration()
+    game = Game(boats1, boats2)
+    
+    #création de socket TCP
+    data = socket.socket(family = socket.AF_INET6, type = socket.SOCK_STREAM, proto = 0, fileno = None)
+    data.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    data.bind(('',7777))
+    data.listen(5)
+
+    clients_connectes = []
+    
+    connexions_demandees, wlist, xlist = select.select([data],[], [], 0.05)
+    
+    for connexion in connexions_demandees:
+        connexion_avec_client, infos_connexion = connexion.accept()
+        # On ajoute le socket connecté à la liste des clients
+        clients_connectes.append(connexion_avec_client)
+
+    # Maintenant, on écoute la liste des clients connectés
+    # Les clients renvoyés par select sont ceux devant être lus (recv)
+    # On attend là encore 50ms maximum
+    # On enferme l'appel à select.select dans un bloc try
+    # En effet, si la liste de clients connectés est vide, une exception
+    # Peut être levée
+    clients_a_lire = []
+    try:
+        clients_a_lire, wlist, xlist = select.select(clients_connectes,
+                [], [], 0.05)
+    except select.error:
+        pass
+    
+    c.send(game)
+    currentPlayer = 0
+    c.send(currentPlayer)
+    while gameOver(game) == -1:
+        (c,a) = data.accept()
+        (x,y,currentPlayer) = c.recv(1500)
+        
+        if (currentPlayer == J0):
+            c.send((x,y,currenPlayer+1))
+        else:
+            c.send((x,y,currentPlayer+1))
+        addShot(game, x, y, currentPlayer)
+
+main()
